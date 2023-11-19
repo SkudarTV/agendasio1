1267e00d4afc3a39fbffece86b89bd948b64d5fd
usersio <erwan@gallenne.fr>
2023-11-19 17:12:37 +0100

    widget "Add Task"

---
+++ resources/views/filament/resources/task-resource/widgets/add-task.blade.php
@@ -0,0 +1,7 @@
+<x-filament-widgets::widget>
+    <x-filament::section>
+        <x-filament::link href="{{ \App\Filament\Resources\TaskResource::getUrl('create') }}">
+            Créer une tâche
+        </x-filament::link>
+    </x-filament::section>
+</x-filament-widgets::widget>
