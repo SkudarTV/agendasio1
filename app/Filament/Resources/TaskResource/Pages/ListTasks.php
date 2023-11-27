7938e86042217ae21f5e0ba3b9ef77eafb64cd2a
usersio <erwan@gallenne.fr>
2023-11-27 07:42:39 +0100

    add tabs "Prochaines" and "Passées" to tasks index

--- app/Filament/Resources/TaskResource/Pages/ListTasks.php
+++ app/Filament/Resources/TaskResource/Pages/ListTasks.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\TaskResource\Pages;
 
 use App\Filament\Resources\TaskResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListTasks extends ListRecords
 {
@@ -14,6 +16,16 @@ class ListTasks extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs()
+    {
+        return [
+            'Prochaines'=>Tab::make('Prochaines')
+                ->modifyQueryUsing(fn(Builder $query)=>$query->where('dueDate','>=',date('Y-m-d'))),
+            'Passées'=>Tab::make('Passées')
+                ->modifyQueryUsing(fn(Builder $query)=>$query->where('dueDate','<',date('Y-m-d')))
         ];
     }
 }
